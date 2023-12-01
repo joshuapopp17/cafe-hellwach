c79ac6be3673a3b545880541f9ee97f47945a0f8
joshuapopp17 <joshuapopp17@csu.fullerton.edu>
2023-11-30 19:55:01 -0800

    made some style adjustments on HOME

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
             <section>
                 <Container  className='hero-container' style={{backgroundColor: '#594A42'}}>
                     <h1 className='hero-caption story-container-text'>A PLAYFUL CAFE IN THE HEART OF BERLIN</h1>
-                    <h2 className='hero-info story-container-text'>145 Krümmer Str., Berlin</h2>
+                    <h2 className='hero-info story-container-text' style={{marginTop: 40}}>145 Krümmer Str., Berlin</h2>
                     <h2 className='hero-info story-container-text'>Mon - Sat</h2>
                     <h2 className='hero-info story-container-text'>7am - 8pm</h2>
                 </Container>
@@ -31,7 +31,7 @@ const Home = () => {
                     <Col sm={6}>
                         <div className='photo-caption'>
                             <h3 className='photo-caption-text'>We have mastered the craft for over 15 years.</h3>
-                            <a onClick={() => navigate(`/menu`)}><h2 className='button'>View our menu</h2></a>
+                            <a onClick={() => navigate(`/menu`)} style={{marginTop: 20}}><h2 className='button' style={{borderRadius: 5}}>View our menu</h2></a>
                         </div>
                     </Col>
                 </Row>
@@ -39,7 +39,7 @@ const Home = () => {
                     <Col className="order-last order-sm-first" sm={6}>
                         <div className='photo-caption'>
                             <h3 className='photo-caption-text'>Quality in house roasts guarantee a fresh and exciting flavor.</h3>
-                            <a onClick={() => navigate(`/store`)}><h2 className='button'>View in store</h2></a>
+                            <a onClick={() => navigate(`/store`)} style={{marginTop: 20}}><h2 className='button' style={{borderRadius: 5}}>View in store</h2></a>
                         </div>
                     </Col>
                     <Col sm={6}><Image src={beans} className='image-row'/></Col>
@@ -50,7 +50,7 @@ const Home = () => {
             <Container  className='story-container' style={{backgroundColor: '#594A42'}}>
                 <AnimationOnScroll animateOnce={true} animateIn="animate__fadeIn">
                     <h1 className='story-container-text sc-header'>Story</h1>
-                    <h4 className='story-container-text sc-text'>We started making coffee in 2008, and have never stopped since.
+                    <h4 className='story-container-text sc-text' style={{lineHeight: 1.5}}>We started making coffee in 2008, and have never stopped since.
                         Coming from a creative background as artists we decided to take an
                         artsy and playful approach. The goal was to create a space where
                         creatives can come together and work in the same space and share their crafts.
@@ -60,7 +60,7 @@ const Home = () => {
                 </AnimationOnScroll>
                 <AnimationOnScroll animateOnce={true} animateIn="animate__fadeIn">
                     <h1 style={{marginTop: 50}} className='story-container-text sc-header'>Our Guarantee</h1>
-                    <h4 className='story-container-text sc-text'>
+                    <h4 className='story-container-text sc-text' style={{lineHeight: 1.5}}>
                         One thing we strive for is 100% ethically sourced ingredients. For this reason,
                         all our products come from small businesses that prioritize human rights and the environment.
                         Lastly, 10% of our profits go to organizations that fight for just working conditions in developing nations.
@@ -70,13 +70,6 @@ const Home = () => {
             </Container>
         </section>
         <Image src={seats} className='image-people'/>
-        <section>
-            <div className='eyes-container'>
-                <Image src={eye} className='eyes-image'/>
-                <Image src={eye} className='eyes-image'/>
-            </div>
-            <h2 className='eyes-text'>Stay creative, stay hellwach.</h2>
-        </section>
     </div>
   )
 }
